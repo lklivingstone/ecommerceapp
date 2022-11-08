da39b53e616f900ead42e49d312866ddcccc8457
L K Livingstone <livingstone.lk.337@gmail.com>
2022-11-08 20:37:59 +0530

    Tested. Deployment pending

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require("express")
+const cors= require("cors")
 const app= express()
 const mongoose= require("mongoose")
 require("dotenv/config")
@@ -18,6 +19,7 @@ app.get("/", (req, res)=> {
 })
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
     process.env.DB_CONNECTION
